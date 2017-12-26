2bf0c899a3b9f2932026b5065c15b8832b4449fb
Sanoj <varghesesanoj@gmail.com>
2017-12-26 16:40:46 +1100

    Update portfolio code with Lambda

--- upload_portfolio_lambda.py
+++ upload_portfolio_lambda.py
@@ -3,17 +3,22 @@ import StringIO
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3')
-portfolio_bucket = s3.Bucket('portfolio.sanoj.info')
+def lambda_handler(event, context):
 
-build_bucket = s3.Bucket('portfoliobuild.sanoj.info')
+    s3 = boto3.resource('s3')
+    portfolio_bucket = s3.Bucket('portfolio.sanoj.info')
+    build_bucket = s3.Bucket('portfoliobuild.sanoj.info')
+    portfolio_zip = StringIO.StringIO()
+    build_bucket.download_fileobj('portfoliobuild.zip',portfolio_zip)
 
-portfolio_zip = StringIO.StringIO()
-build_bucket.download_fileobj('portfoliobuild.zip',portfolio_zip)
 
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj, nm,
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+         obj = myzip.open(nm)
+         portfolio_bucket.upload_fileobj(obj, nm,
             ExtraArgs={'ContentType':mimetypes.guess_type(nm)[0]})
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+         portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+    print "Done"
+
+    return 'Hello from Lambda'
